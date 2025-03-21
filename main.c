c1322604fbb72c13bb52aa6227947a15708e4f68
Ander Ayesta <ander.ayesta@opendeusto.es>
2025-03-21 08:43:26 +0100

    Creacion de la bd y incluir los .c y .h de sqlite3

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include "operaciones.h"
+#include "sqlite3.h"
+#include <stdlib.h>
 
 int main (void){
-	printf("Hola mundo");
-	printf("AYUDA, no entiendo nada");
+	sqlite3 * db;
+	int abrir = sqlite3_open("adate.db", &db);
+
+	if (abrir) {
+	    printf("Error al conectar a la bd: %s\n", sqlite3_errmsg(db));
+	    return 1;
+	} else {
+	    printf("Conectado a la bd\n");
+	}
+
+	sqlite3_close(db);
 
 }
 
