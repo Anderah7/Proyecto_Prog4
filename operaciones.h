d1e57e4ca2d3ec405aadc0daa1dca1a38997ec1f
Ander Ayesta <ander.ayesta@opendeusto.es>
2025-03-22 14:27:50 +0100

    Creacion de la estructuras necesarias

--- operaciones.h
+++ operaciones.h
@@ -3,6 +3,40 @@
 
 #include "sqlite3.h"
 
+typedef struct {
+	int idProd;
+	char * nombreProd;
+	float precio;
+	int codSeccion;
+	int codProveedor;
+}Producto;
+
+typedef struct {
+	int codProveedor;
+	char * nombreProveedor;
+	int codPostal;
+	char * contrasena;
+}Proveedor;
+
+typedef struct {
+	int codSeccion;
+	char * nombreSeccion;
+}Seccion;
+
+typedef struct {
+	int idDepartamento;
+	char * nombreDepartamento;
+	int NSSJefe;
+}Departamento;
+
+typedef struct {
+	int NSS;
+	char * nombreEmpleado;
+	char * contrasena;
+	int codSeccion;
+	int idDepartamento;
+}Empleado;
+
 void crearTablas(sqlite3 *db);
 
 #endif
